1ec7dfad78b62f573a55dec57f93feb3271c8f57
Rishav <rishavjaiswal864@gmail.com>
2024-09-01 00:25:43 +0530

    emoji added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from 'react'
+import  { useEffect, useRef} from 'react'
 import resume from "../pdf/webResume.pdf"
 import hero from "./data/hero.json"
 import Typed from "typed.js";
@@ -26,7 +26,7 @@ const Home = () => {
         <div className="left"  data-aos="fade-up-left"
       data-aos-duration="1000">
           <h1 ref={typedRef}>
-            Lorem, ipsum dolor sit amet consectetur 
+           Hey there.....👨🏻‍💻 
           </h1>
           <a href={resume} download="Resume.pdf" className="btn btn-outline-warning my-3">
             Resume 
