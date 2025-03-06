3dfc43cb529d2243cd757b16398843c1d4141d45
Rishav <rishavjaiswal864@gmail.com>
2025-03-06 11:54:50 +0530

    added a frontend project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { FaBriefcase } from "react-icons/fa";
+import "../index.css"
 
 const Experience = () => {
   useEffect(() => {
@@ -9,15 +10,12 @@ const Experience = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-6 py-10 bg-gradient-to-b from-gray-900 to-black text-white">
-      <h1 className="text-4xl font-bold text-center mb-8 text-blue-400" data-aos="fade-down">
+    <div className="experience" id="experience">
+      <h1 className="text-4xl font-bold text-center mb-8" data-aos="fade-down">
         Experience
       </h1>
-      <div
-        className="flex justify-center"
-        data-aos="zoom-in"
-      >
-        <div className="bg-gradient-to-r from-indigo-700 to-purple-900 p-6 rounded-lg shadow-lg w-96 text-center transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+      <div className="experience-items">
+        <div className="experience-item" data-aos="zoom-in">
           <FaBriefcase className="text-5xl text-yellow-400 animate-pulse mx-auto" />
           <h2 className="text-2xl font-semibold text-white mt-4">MERN Stack Internship</h2>
           <h3 className="text-xl font-medium text-yellow-300 mt-1">Prep Lab</h3>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             Portfolio</div>
           <div className='right'>
             <a href="#home" className="nav_items">Home</a>
-            <a href="#" className="nav_items">Exprience</a>
+            <a href="#experience" className="nav_items">Experience</a>
             <a href="#skills" className="nav_items">Skills</a>
             <a href="#project" className="nav_items">Projects</a>
             <a href="#contact" className="nav_items">Contact</a>
